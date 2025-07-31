37c647c915fce9617d4c82f2908cfbf59fbeabe9
kai <kai.deuley@bobcats.gcsu.edu>
2025-07-30 21:17:56 -0400

    faces done, now voice

--- client/src/auth/AuthProvider.jsx
+++ client/src/auth/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import { useNavigate } from "react-router-dom";  // Import useNavigate
 
 const AuthContext = createContext();
